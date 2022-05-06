71a24d36256d342cfa47492393feab414f454ee4
Daniel Branco <daniel-cb1@hotmail.com>
2022-05-06 20:23:47 -0300

    5 commit

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #E52E4D;
+        --green: #33cc95; 
         --blue: #5429CC;
 
         --blue-light: #6933FF;
@@ -35,7 +36,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    border-style, input, textarea, button {
+    body, input, textarea, button {
       font-family: 'Poppins', sans-serif;
       font-weight: 400;
     }
