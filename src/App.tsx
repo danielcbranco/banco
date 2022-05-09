e1b1903051c01243dfff4ee7a482a46f722b9c82
Daniel De Assis Castelo Branco <daniel.branco@sefa.pa.gov.br>
2022-05-09 08:19:06 -0300

    9 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
